4c16830980ba16fafa3545a6eefe20ad2b234531
Rene <ramirezrene96@gmail.com>
2018-04-16 15:16:37 -0700

    added ft_putchar, function that writes one character at a time, main uses a for loop to iterate from A - Z

--- ft_putchar.c
+++ ft_putchar.c
@@ -1,24 +1,8 @@
+#include "libft.h"
 #include <stdio.h>
 #include <unistd.h>
 
 void		ft_putchar(char c)
 {
 	write(1, &c, 1);
-}
-
-int			main(void)
-{
-	char letter;
-	char ltts;
-
-	printf("Renes function: \n");
-	for (letter = 'A' ; letter <= 'Z' ; letter++)
-		{ft_putchar(letter);}
-	printf("\n\n");
-	printf("Computers function: \n");
-	for (ltts = 'A' ; ltts <= 'Z' ; ltts++)
-		{putchar(ltts);}
-	printf("\n");
-	
-	return (0);
 }
