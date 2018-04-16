f7eab69a80e424ecbb31db3466f41a69075b5f13
Rene <ramirezrene96@gmail.com>
2018-04-16 16:08:57 -0700

    adding libft.h a library that allows me to call other functions from other places.

---
+++ libft.h
@@ -0,0 +1,10 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void					ft_putstr(char const *s);
+void					ft_putchar(char c);
